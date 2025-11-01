b3846e4bc5e6f2052acc62c2b95870458a295d6b
Sokhom Panha <sokhompanha70@gmail.com>
2025-11-01 07:54:15 +0700

    Exercise4 e-commerce

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+import CategoryComponent from '@/components/CategoryComponent.vue'
+import PosterComponent from '@/components/PosterComponent.vue'
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .component('CategoryComponent', CategoryComponent)
+  .component('PosterComponent', PosterComponent)
+  .mount('#app')
